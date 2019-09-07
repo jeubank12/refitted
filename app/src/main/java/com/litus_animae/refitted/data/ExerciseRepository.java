411d81c3d3f005ca3dda0e1933cb755940097083
Josiah Eubank <eub.kansas19@gmail.com>
2019-09-07 15:46:30 -0500

    Simplify the code block for retrieving exercises

--- app/src/main/java/com/litus_animae/refitted/data/ExerciseRepository.java
+++ app/src/main/java/com/litus_animae/refitted/data/ExerciseRepository.java
@@ -6,21 +6,16 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
-import androidx.room.Room;
 
-import com.litus_animae.refitted.models.Exercise;
 import com.litus_animae.refitted.models.ExerciseRecord;
 import com.litus_animae.refitted.models.ExerciseSet;
 import com.litus_animae.refitted.models.SetRecord;
 import com.litus_animae.refitted.threads.CloseDatabaseRunnable;
-import com.litus_animae.refitted.threads.GetExerciseRunnable;
 import com.litus_animae.refitted.threads.StoreRecordsRunnable;
 
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -32,31 +27,23 @@ public class ExerciseRepository {
     private MutableLiveData<List<ExerciseRecord>> records = new MutableLiveData<>();
 
     public ExerciseRepository(Context context) {
-        applicationContext = new WeakReference<Context>(context.getApplicationContext());
+        applicationContext = new WeakReference<>(context.getApplicationContext());
         threadPoolService = Executors.newCachedThreadPool();
     }
 
     public void loadExercises(String day, String workoutId) {
         LiveData<ExerciseRoom> room = RoomDataService.getExerciseRoomAsync(applicationContext);
-        LiveData<Set<String>> keys = Transformations.switchMap(room, roomDb -> {
-            if (roomDb != null){
-                return Transformations.map(
-                        roomDb.getExerciseDao().getSteps(day, workoutId),
-                        HashSet::new);
+        exercises.addSource(Transformations.switchMap(room, roomDb -> {
+            if (roomDb != null) {
+                return Transformations.switchMap(
+                        Transformations.map(
+                                roomDb.getExerciseDao().getSteps(day, workoutId),
+                                HashSet::new),
+                        stepKeys -> roomDb.getExerciseDao().getExerciseSets(day, workoutId, stepKeys.toArray(new String[0]))
+                );
             }
             return new MutableLiveData<>();
-        });
-        exercises.addSource(Transformations.switchMap(keys, stepKeys -> {
-            if (stepKeys != null){
-                return Transformations.switchMap(room, roomDb -> {
-                    if (roomDb != null) {
-                        return roomDb.getExerciseDao().getExerciseSets(day, workoutId, stepKeys.toArray(new String[0]));
-                    }
-                    return new MutableLiveData<>();
-                });
-            }
-            return new MutableLiveData<>();
-        }), loadedExercises -> exercises.setValue(loadedExercises));
+        }), exerciseSets -> exercises.setValue(exerciseSets));
 //        threadPoolService.submit(new GetExerciseRunnable(applicationContext.get(), exercises, records,
 //                day, workoutId));
     }
@@ -73,7 +60,7 @@ public class ExerciseRepository {
         return records;
     }
 
-    public void shutdown(){
+    public void shutdown() {
         threadPoolService.submit(new CloseDatabaseRunnable(applicationContext.get()));
         threadPoolService.shutdown();
     }
