7d74f60ae7bd9c80ddce7792d993472813c292e9
Josiah Eubank <eub.kansas19@gmail.com>
2020-08-03 13:05:35 -0600

    Alternate exercise unit test

--- app/src/test/java/com/litus_animae/refitted/util/TestLogUtil.kt
+++ app/src/test/java/com/litus_animae/refitted/util/TestLogUtil.kt
@@ -2,18 +2,18 @@ package com.litus_animae.refitted.util
 
 object TestLogUtil : LogUtil {
     override fun d(tag: String, msg: String) {
-        //TODO("Not yet implemented")
+        println("DEBUG: $tag$msg")
     }
 
     override fun w(tag: String, msg: String) {
-        //TODO("Not yet implemented")
+        println("WARN: $tag$msg")
     }
 
     override fun e(tag: String, msg: String) {
-        //TODO("Not yet implemented")
+        println("ERROR: $tag$msg")
     }
 
     override fun e(tag: String, msg: String, ex: Throwable) {
-        //TODO("Not yet implemented")
+        println("ERROR: $tag$msg ${ex.message}")
     }
 }
