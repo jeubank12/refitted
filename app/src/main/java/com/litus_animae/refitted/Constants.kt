ac8e1998114dce3c77ecf0e16b42e490ba062d71
Josiah Eubank <eub.kansas19@gmail.com>
2020-08-07 15:05:15 -0600

    Update more kotlin

---
+++ app/src/main/java/com/litus_animae/refitted/Constants.kt
@@ -0,0 +1,6 @@
+package com.litus_animae.refitted
+
+object Constants {
+    const val EXERCISE_LOAD_SUCCESS = 100
+    const val EXERCISE_LOAD_FAIL = 101
+}
---
+++ app/src/main/java/com/litus_animae/refitted/WorkoutCalendarViewActivity.kt
@@ -0,0 +1,164 @@
+package com.litus_animae.refitted
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.Switch
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.litus_animae.refitted.models.ExerciseViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import java.lang.ref.WeakReference
+
+@AndroidEntryPoint
+class WorkoutCalendarViewActivity : AppCompatActivity() {
+    private var googleSignInOptions: GoogleSignInOptions? = null
+    private var mAuth: FirebaseAuth? = null
+    private var viewModel: ExerciseViewModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val model: ExerciseViewModel by viewModels()
+        viewModel = model
+        setContentView(R.layout.activity_workout_calendar_view)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        (findViewById<View>(R.id.toolbar_layout) as CollapsingToolbarLayout).title = "Athlean-X"
+        val planSwitch = findViewById<Switch>(R.id.switch1)
+        planSwitch.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            val prefs = getSharedPreferences("RefittedMainPrefs", Context.MODE_PRIVATE)
+            val editor = prefs.edit()
+            if (isChecked) {
+                editor.putString("workout", "Inferno Size")
+            } else {
+                editor.putString("workout", "AX1")
+            }
+            editor.apply()
+        }
+        val prefs = getSharedPreferences("RefittedMainPrefs", Context.MODE_PRIVATE)
+        planSwitch.isChecked = prefs.getString("workout", "AX1") != "AX1"
+        val list = findViewById<RecyclerView>(R.id.calendar_recycler)
+        list.adapter = CalendarAdapter(84, WeakReference(planSwitch))
+        list.layoutManager = LinearLayoutManager(this)
+        mAuth = FirebaseAuth.getInstance()
+        googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .build()
+        val currentUser = mAuth!!.currentUser
+        if (currentUser != null) {
+            updateUI(currentUser)
+        } else {
+            val mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions!!)
+            val silentSignInTask = mGoogleSignInClient.silentSignIn()
+            if (silentSignInTask.isSuccessful) {
+                handleSignInResult(silentSignInTask)
+            } else {
+                silentSignInTask.addOnCompleteListener { task -> handleSignInResult(task) }
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct!!.id)
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task: Task<AuthResult?> ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+                        val user = mAuth!!.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                        //updateUI(null);
+                        doFullSignIn()
+                    }
+                }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == GoogleSignInStatusCodes.SUCCESS) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            firebaseAuthWithGoogle(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+
+            // just loop
+            doFullSignIn()
+        }
+    }
+
+    private fun doFullSignIn() {
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions!!)
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, GoogleSignInStatusCodes.SUCCESS)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val prefs = getSharedPreferences("RefittedMainPrefs", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("lastActivity", javaClass.name)
+        editor.apply()
+    }
+
+    private fun updateUI(account: FirebaseUser?) {
+        try {
+            val prefs = getSharedPreferences("RefittedMainPrefs", Context.MODE_PRIVATE)
+            val activityClass = Class.forName(
+                    prefs.getString("lastActivity", javaClass.name)!!)
+            val intent = Intent(this, activityClass)
+            val workout = prefs.getString("workout", "-1")!!
+            val day = prefs.getInt("day", -1)
+            intent.putExtra("workout", workout)
+            intent.putExtra("day", day)
+            if (activityClass != javaClass && day > 0 && workout != "-1") {
+                startActivity(intent)
+            }
+        } catch (ex: ClassNotFoundException) {
+            Log.e(TAG, "onCreate: bad class reference in shared preferences", ex)
+        } catch (ex: Exception) {
+            Log.e(TAG, "onCreate: shared preferences error", ex)
+        }
+    }
+
+    companion object {
+        private const val TAG = "WorkoutCalendarViewActi"
+    }
+}
