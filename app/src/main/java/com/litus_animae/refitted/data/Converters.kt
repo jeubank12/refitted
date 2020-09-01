a96305ba110ba851439221d1e724406f01ac96de
Josiah Eubank <eub.kansas19@gmail.com>
2020-09-01 10:15:52 -0600

    Everything except fragments is ready

---
+++ app/src/main/java/com/litus_animae/refitted/data/Converters.kt
@@ -0,0 +1,18 @@
+package com.litus_animae.refitted.data
+
+import androidx.room.TypeConverter
+import java.util.*
+
+object Converters {
+    @JvmStatic
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return if (value == null) null else Date(value)
+    }
+
+    @JvmStatic
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
