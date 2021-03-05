b0dbbf19f3aa64aaa3d214f4b712857ba04f6400
Josiah Eubank <eub.kansas19@gmail.com>
2021-03-05 15:25:57 -0700

    [jetpack-compose-conversion] updated calendar - wip

--- app/src/main/java/com/litus_animae/refitted/compose/CalendarComposable.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/CalendarComposable.kt
@@ -1,21 +1,27 @@
 package com.litus_animae.refitted.compose
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class CalendarComposable {
 
     @Composable
     fun Calendar(days: Map<Int, Boolean>, daysPerRow: Int = 7) {
-        days.keys.sorted().chunked(daysPerRow).map{ chunk ->
-            Row {
-                chunk.map{
-                    Column {
-                        CalendarDayButton(it, days.get(it))
+        Column(Modifier.fillMaxWidth().padding(10.dp, 10.dp)) {
+            days.keys.sorted().chunked(daysPerRow).map { chunk ->
+                Row(Modifier.fillMaxWidth()) {
+                    chunk.map {
+                        Column(Modifier.weight(1f).height(75.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+                            CalendarDayButton(it, days.get(it))
+                        }
                     }
                 }
             }
@@ -24,14 +30,15 @@ class CalendarComposable {
 
     @Composable
     fun CalendarDayButton(day: Int, isComplete: Boolean?){
-        Button(onClick = { /*TODO*/ }) {
-            Text("$day")
+        Button(onClick = { /*TODO*/ },
+        Modifier.padding(1.dp, 8.dp)) {
+            Text(String.format("%d", day))
         }
     }
 
-    @Preview
+    @Preview(showSystemUi = true)
     @Composable
-    fun previewCalendar() {
-        Calendar((1..28).map{it to true}.toMap())
+    fun PreviewCalendar() {
+        Calendar((1..25).map{it to true}.toMap())
     }
 }
