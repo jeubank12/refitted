638addf3881ddba70b728d2f01830898d61f35d9
Josiah Eubank <eub.kansas19@gmail.com>
2021-03-08 12:37:54 -0700

    [jetpack-compose-conversion] Finalize the basic calendar view

--- app/src/main/java/com/litus_animae/refitted/compose/CalendarComposable.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/CalendarComposable.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.litus_animae.refitted.ExerciseDetailViewActivity
@@ -65,10 +66,10 @@ object CalendarComposable {
 //            }
                 intent.putExtra("day", day)
                 context.startActivity(intent)
-                      },
-            Modifier.padding(1.dp, 8.dp)
+                      }
         ) {
-            Text(String.format("%d", day))
+            Text(String.format("%d", day),
+            textAlign = TextAlign.Center)
         }
     }
 }
--- app/src/main/java/com/litus_animae/refitted/compose/Layout.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/Layout.kt
@@ -1,23 +1,35 @@
 package com.litus_animae.refitted.compose
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.litus_animae.refitted.compose.Layout.Main
 
 object Layout {
-    
+
     @Composable
     fun Main() {
         Scaffold(topBar = {
-            TopAppBar {
-                Text("Athlean-X")
-            }
+            TopAppBar(
+                title = { Text("Athlean-X") },
+                backgroundColor = MaterialTheme.colors.primary
+            )
         }) {
             CalendarComposable.Calendar(
                 days = 84,
                 dayStatus = emptyMap()
             )
         }
+    }
+}
+
+class LayoutCompose {
+    @Preview
+    @Composable
+    fun PreviewMain() {
+        Main()
     }
 }
