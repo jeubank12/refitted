b4b692391d805035e67a753782275fb2a4495bf4
Josiah Eubank <eub.kansas19@gmail.com>
2021-12-31 17:49:05 -0700

    [all-in-compose] Fix loading and calendar

--- app/src/main/java/com/litus_animae/refitted/compose/LoadingScreen.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/LoadingScreen.kt
@@ -1,11 +1,15 @@
 package com.litus_animae.refitted.compose
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun LoadingView() {
-    Text("Loading...")
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        CircularProgressIndicator()
+    }
 }
