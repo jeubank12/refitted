4c4f6dbd86cca3edd9bd731706b66e81177d2c7b
Josiah Eubank <eub.kansas19@gmail.com>
2022-01-02 12:27:11 -0700

    [all-in-compose] Add weight and reps buttons to exercise set details

--- app/src/main/java/com/litus_animae/refitted/compose/ExerciseSet.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/ExerciseSet.kt
@@ -1,25 +1,40 @@
 package com.litus_animae.refitted.compose
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.litus_animae.refitted.R
+import com.litus_animae.refitted.compose.state.Repetitions
+import com.litus_animae.refitted.compose.state.Weight
 
 @Composable
 fun ExerciseSetView(currentIndex: Int, maxIndex: Int, updateIndex: (Int) -> Unit) {
-    Column(Modifier.fillMaxSize()) {
-        Row(Modifier.weight(3f)) {}
-        Row(
-            Modifier
-                .weight(1f)
-                .fillMaxWidth()) {
-            Column(Modifier.weight(1f)) {
+    val weight = remember { Weight(0.0) }
+    val reps = remember { Repetitions(0) }
+    Column {
+        Row(Modifier.weight(3f)) {
+            Column(Modifier.weight(4f)) {
+                WeightButtons(weight)
+            }
+            Column(Modifier.weight(1f), horizontalAlignment = Alignment.End) {
+                RepetitionsButtons(reps)
+            }
+        }
+        Row(Modifier.weight(1f)) {
+            Column(
+                Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            ) {
                 val enabled = currentIndex > 0
                 Button(
                     onClick = { updateIndex(currentIndex - 1) },
@@ -29,9 +44,12 @@ fun ExerciseSetView(currentIndex: Int, maxIndex: Int, updateIndex: (Int) -> Unit
                     Text(text)
                 }
             }
-            Column(Modifier.weight(3f)) {
-            }
-            Column(Modifier.weight(1f)) {
+            Column(Modifier.weight(3f)) { }
+            Column(
+                Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            ) {
                 val enabled = currentIndex < maxIndex
                 Button(
                     onClick = { updateIndex(currentIndex + 1) },
@@ -41,6 +59,27 @@ fun ExerciseSetView(currentIndex: Int, maxIndex: Int, updateIndex: (Int) -> Unit
                     Text(text)
                 }
             }
+        }
+        Row(Modifier.weight(1f)) {
+            Button(onClick = {}, Modifier.fillMaxWidth()) {
+                val storeWeight by weight.value
+                val storeReps by reps.value
+                Text("Will store: $storeWeight lbs $storeReps reps")
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(heightDp = 400)
+fun PreviewExerciseSetDetails() {
+    MaterialTheme(Theme.lightColors) {
+        Column(
+            Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            ExerciseSetView(currentIndex = 5, maxIndex = 5) { }
         }
     }
 }
