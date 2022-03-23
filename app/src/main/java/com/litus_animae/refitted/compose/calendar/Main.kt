ab4af54d75f2c4d7d3ef807179978d2824a06ad1
Josiah Eubank <eub.kansas19@gmail.com>
2022-03-23 12:42:31 -0600

    [calendar-completion] styling

--- app/src/main/java/com/litus_animae/refitted/compose/calendar/Main.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/calendar/Main.kt
@@ -27,116 +27,120 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalCoroutinesApi::class)
 @Composable
 fun Calendar(
-    navigateToWorkoutDay: (WorkoutPlan, Int) -> Unit,
-    model: WorkoutViewModel = viewModel(),
+  navigateToWorkoutDay: (WorkoutPlan, Int) -> Unit,
+  model: WorkoutViewModel = viewModel(),
 ) {
-    val scaffoldState = rememberScaffoldState()
-    val scaffoldScope = rememberCoroutineScope()
-    val coroutineScope = rememberCoroutineScope()
+  val scaffoldState = rememberScaffoldState()
+  val scaffoldScope = rememberCoroutineScope()
+  val coroutineScope = rememberCoroutineScope()
 
-    val selectedWorkoutPlan by model.currentWorkout.collectAsState(
-        initial = model.savedStateLastWorkoutPlan,
-        Dispatchers.IO
-    )
-    val savedSelectedPlanLoading = model.savedStateLoading
-    val completedDaysLoading = model.completedDaysLoading
+  val selectedWorkoutPlan by model.currentWorkout.collectAsState(
+    initial = model.savedStateLastWorkoutPlan,
+    Dispatchers.IO
+  )
+  val savedSelectedPlanLoading = model.savedStateLoading
+  val completedDaysLoading = model.completedDaysLoading
 
-    val completedDays by model.completedDays.collectAsState(initial = emptyMap(), Dispatchers.IO)
+  val completedDays by model.completedDays.collectAsState(initial = emptyMap(), Dispatchers.IO)
 
-    Scaffold(
-        scaffoldState = scaffoldState,
-        topBar = {
-            TopAppBar(
-                title = {
-                    selectedWorkoutPlan?.let { Text(it.workout) } ?: Text("Refitted")
-                },
-                backgroundColor = MaterialTheme.colors.primary,
-                navigationIcon = {
-                    Icon(
-                        Icons.Default.Menu,
-                        // TODO localize
-                        "menu",
-                        modifier = Modifier
-                            .clickable {
-                                scaffoldScope.launch {
-                                    if (scaffoldState.drawerState.isClosed) scaffoldState.drawerState.open()
-                                    else scaffoldState.drawerState.close()
-                                }
-                            }
-                            .padding(start = 10.dp))
-                },
-                actions = {
-                    if (selectedWorkoutPlan != null) {
-                        val (expanded, setExpanded) = rememberSaveable { mutableStateOf(false) }
-                        val (alerted, setAlerted) = rememberSaveable { mutableStateOf(false) }
-                        // TODO localize
-                        Icon(Icons.Default.MoreVert, "workout menu",
-                            modifier = Modifier.clickable { setExpanded(!expanded) })
-                        DropdownMenu(
-                            expanded = expanded,
-                            onDismissRequest = { setExpanded(false) }) {
-                            Text("Reset workout",
-                                Modifier
-                                    .fillMaxWidth()
-                                    .clickable {
-                                        setAlerted(true)
-                                        setExpanded(false)
-                                    })
-                        }
-                        if (alerted) {
-                            AlertDialog(onDismissRequest = { setAlerted(false) },
-                                // TODO localize
-                                title = { Text("Reset Workout Completion") },
-                                text = { Text("This will reset your completed days. Are you sure? (This does not remove records of your previous exercise sets") },
-                                confirmButton = {
-                                    Button(onClick = {
-                                        model.resetWorkoutCompletion(
-                                            selectedWorkoutPlan!!
-                                        )
-                                        setAlerted(false)
-                                    }) {
-                                        Text("Yes")
-                                    }
-                                },
-                                dismissButton = {
-                                    Button(onClick = { setAlerted(false) }) {
-                                        Text("No")
-                                    }
-                                })
-                        }
+  Scaffold(
+    scaffoldState = scaffoldState,
+    topBar = {
+      TopAppBar(
+        title = {
+          selectedWorkoutPlan?.let { Text(it.workout) } ?: Text("Refitted")
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        navigationIcon = {
+          Icon(
+            Icons.Default.Menu,
+            // TODO localize
+            "menu",
+            modifier = Modifier
+                .clickable {
+                    scaffoldScope.launch {
+                        if (scaffoldState.drawerState.isClosed) scaffoldState.drawerState.open()
+                        else scaffoldState.drawerState.close()
                     }
                 }
-            )
+                .padding(start = 10.dp))
         },
-        drawerShape = MaterialTheme.shapes.medium,
-        drawerContent = {
-            val workoutPlanPagingItems = model.workouts.collectAsLazyPagingItems()
-            WorkoutPlanMenu(workoutPlanPagingItems) {
-                scaffoldScope.launch { scaffoldState.drawerState.close() }
-                coroutineScope.launch { model.loadWorkoutDaysCompleted(it) }
+        actions = {
+          if (selectedWorkoutPlan != null) {
+            val (expanded, setExpanded) = rememberSaveable { mutableStateOf(false) }
+            val (alerted, setAlerted) = rememberSaveable { mutableStateOf(false) }
+            // TODO localize
+            Icon(Icons.Default.MoreVert, "workout menu",
+              modifier = Modifier
+                  .clickable { setExpanded(!expanded) }
+                  .padding(end = 10.dp))
+            DropdownMenu(
+              expanded = expanded,
+              onDismissRequest = { setExpanded(false) }) {
+              Text("Reset workout",
+                  Modifier
+                      .fillMaxWidth()
+                      .clickable {
+                          setAlerted(true)
+                          setExpanded(false)
+                      }
+                      .padding(start = 5.dp, end = 15.dp)
+                      .padding(vertical = 5.dp))
             }
-        }) {
-        if (savedSelectedPlanLoading || completedDaysLoading) {
-            Surface(Modifier.fillMaxSize()) {
-                LoadingView()
+            if (alerted) {
+              AlertDialog(onDismissRequest = { setAlerted(false) },
+                // TODO localize
+                title = { Text("Reset Workout Completion") },
+                text = { Text("This will reset your completed days. Are you sure? (This does not remove records of your previous exercise sets") },
+                confirmButton = {
+                  Button(onClick = {
+                    model.resetWorkoutCompletion(
+                      selectedWorkoutPlan!!
+                    )
+                    setAlerted(false)
+                  }) {
+                    Text("Yes")
+                  }
+                },
+                dismissButton = {
+                  Button(onClick = { setAlerted(false) }) {
+                    Text("No")
+                  }
+                })
             }
-        } else if (selectedWorkoutPlan == null) {
-            // TODO instruction page
-            Row(
-                Modifier
-                    .padding(start = 10.dp, top = 10.dp)
-                    .fillMaxWidth()
-            ) {
-                Text("Open the menu to pick a workout")
-            }
-        } else {
-            WorkoutCalendar(
-                selectedWorkoutPlan!!,
-                completedDays
-            ) {
-                navigateToWorkoutDay(selectedWorkoutPlan!!, it)
-                coroutineScope.launch { model.setLastViewedDay(selectedWorkoutPlan!!, it) }
-            }
+          }
         }
+      )
+    },
+    drawerShape = MaterialTheme.shapes.medium,
+    drawerContent = {
+      val workoutPlanPagingItems = model.workouts.collectAsLazyPagingItems()
+      WorkoutPlanMenu(workoutPlanPagingItems) {
+        scaffoldScope.launch { scaffoldState.drawerState.close() }
+        coroutineScope.launch { model.loadWorkoutDaysCompleted(it) }
+      }
+    }) {
+    if (savedSelectedPlanLoading || completedDaysLoading) {
+      Surface(Modifier.fillMaxSize()) {
+        LoadingView()
+      }
+    } else if (selectedWorkoutPlan == null) {
+      // TODO instruction page
+      Row(
+          Modifier
+              .padding(start = 10.dp, top = 10.dp)
+              .fillMaxWidth()
+      ) {
+        Text("Open the menu to pick a workout")
+      }
+    } else {
+      WorkoutCalendar(
+        selectedWorkoutPlan!!,
+        completedDays
+      ) {
+        navigateToWorkoutDay(selectedWorkoutPlan!!, it)
+        coroutineScope.launch { model.setLastViewedDay(selectedWorkoutPlan!!, it) }
+      }
     }
+  }
 }
