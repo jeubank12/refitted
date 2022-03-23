89a1e3eb6d2fe1422e9f2892a51dc4dba9b18159
Josiah Eubank <eub.kansas19@gmail.com>
2022-03-22 18:08:17 -0600

    [remote-cache-exercises] Committing before abandoning this approach

--- app/src/main/java/com/litus_animae/refitted/data/room/ExerciseSetRemoteMediator.kt
+++ app/src/main/java/com/litus_animae/refitted/data/room/ExerciseSetRemoteMediator.kt
@@ -19,43 +19,52 @@ class ExerciseSetRemoteMediator(
 
     private val compareByStep = Comparator.comparing(RoomExerciseSet::step)
 
-    fun exerciseSets(dayAndWorkout: DayAndWorkout): Flow<List<ExerciseSet>> {
+    private fun exerciseSteps(dayAndWorkout: DayAndWorkout): Flow<List<String>> {
         return flow {
             val (workoutDay, workoutId) = dayAndWorkout
             log.i(TAG, "Checking cache for $dayAndWorkout")
             val cachedSteps = exerciseDao.loadSteps(workoutDay, workoutId)
             if (cachedSteps.isEmpty()) {
                 log.i(TAG, "Did not find cache for $dayAndWorkout, loading from network service")
-                val networkSets = networkService.getExerciseSets(dayAndWorkout)
-                log.d(TAG, "Emitting network results then storing to cache: $networkSets")
-                emit(networkSets)
-                log.d(TAG, "Storing to cache: $networkSets")
-                networkSets.map {
-                    val maybeExercise = it.exercise.firstOrNull()
-                    log.d(TAG, "Saving ${maybeExercise}, $it")
-                    maybeExercise?.let { exercise ->
-                        exerciseDao.storeExerciseAndSet(exercise, RoomExerciseSet(it))
-                    }
-                }
+                emitAll(loadNetworkSets(dayAndWorkout))
                 log.d(TAG, "Transitioning to live query")
             } else {
                 log.i(TAG, "Found cache for $dayAndWorkout")
             }
             val liveSteps = exerciseDao.getSteps(workoutDay, workoutId)
-            val liveSets = liveSteps
-                .flatMapLatest {
-                    log.d(TAG, "Steps updated, reloading exercise sets")
-                    exerciseDao.getExerciseSets(workoutDay, workoutId, *it.toTypedArray())
-                }.map {
-                    log.d(TAG, "Sets updated, converting to model")
-                    it.sortedWith(compareByStep).map { set ->
-                        log.d(TAG, "Loading ${set.name} ${set.workout}")
-                        val exercise = exerciseDao.getExercise(set.name, set.workout)
-                        ExerciseSet(set, exercise.flowOn(Dispatchers.IO))
-                    }
+            emitAll(liveSteps)
+        }
+    }
+
+    private suspend fun loadNetworkSets(dayAndWorkout: DayAndWorkout): Flow<List<String>> {
+        val networkSets = networkService.getExerciseSets(dayAndWorkout)
+        log.d(TAG, "Emitting network results then storing to cache: $networkSets")
+        log.d(TAG, "Storing to cache: $networkSets")
+        return flowOf(networkSets
+            .map {
+                val maybeExercise = it.exercise.firstOrNull()
+                log.d(TAG, "Saving ${maybeExercise}, $it")
+                maybeExercise?.let { exercise ->
+                    exerciseDao.storeExerciseAndSet(exercise, RoomExerciseSet(it))
                 }
-            emitAll(liveSets)
-        }.flowOn(Dispatchers.IO)
+                it.step
+            })
+    }
+
+    fun exerciseSets(dayAndWorkout: DayAndWorkout): Flow<List<ExerciseSet>> {
+        val (workoutDay, workoutId) = dayAndWorkout
+        return exerciseSteps(dayAndWorkout)
+            .flatMapLatest {
+                log.d(TAG, "Steps updated, reloading exercise sets")
+                exerciseDao.getExerciseSets(workoutDay, workoutId, *it.toTypedArray())
+            }.map {
+                log.d(TAG, "Sets updated, converting to model")
+                it.sortedWith(compareByStep).map { set ->
+                    log.d(TAG, "Loading ${set.name} ${set.workout}")
+                    val exercise = exerciseDao.getExercise(set.name, set.workout)
+                    ExerciseSet(set, exercise.flowOn(Dispatchers.IO))
+                }
+            }
     }
 
     companion object {
