04f21afaeca469f567ae222190155feb4b3747fb
Josiah Eubank <eub.kansas19@gmail.com>
2022-03-24 19:07:33 -0600

    [quality] Use constrained button in reps buttons

--- app/src/main/java/com/litus_animae/refitted/compose/exercise/input/RepetitionsButtons.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/exercise/input/RepetitionsButtons.kt
@@ -10,59 +10,70 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import com.litus_animae.refitted.compose.state.Repetitions
+import com.litus_animae.refitted.compose.util.ConstrainedButton
 import com.litus_animae.refitted.compose.util.Theme
 
 @Composable
 fun RepetitionsButtons(reps: Repetitions) {
-    val currentReps by reps.value
-    Row(Modifier.padding(vertical = 5.dp), horizontalArrangement = Arrangement.SpaceAround) {
-        RepetitionButton(1, reps::plus)
-    }
-    Row(
+  val currentReps by reps.value
+  BoxWithConstraints() {
+    // TODO if available size is too small.....
+    val maxButtonWidth = maxWidth - 10.dp
+    val maxButtonHeight = (maxHeight - 30.dp) / 3
+    val availableSize = min(maxButtonWidth, maxButtonHeight)
+    val buttonSize = min(availableSize, 50.dp)
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+      Row(Modifier.padding(top = 5.dp), horizontalArrangement = Arrangement.SpaceAround) {
+        RepetitionButton(1, reps::plus, buttonSize)
+      }
+      Row(
         Modifier
-            .size(50.dp)
-            .padding(10.dp),
+          .size(buttonSize)
+          .padding(10.dp),
         verticalAlignment = Alignment.Bottom,
         horizontalArrangement = Arrangement.Center
-    ) {
+      ) {
         Text(currentReps.toString())
+      }
+      Row(Modifier.padding(bottom = 5.dp), horizontalArrangement = Arrangement.SpaceAround) {
+        RepetitionButton(-1, reps::plus, buttonSize)
+      }
     }
-    Row(Modifier.padding(vertical = 5.dp), horizontalArrangement = Arrangement.SpaceAround) {
-        RepetitionButton(-1, reps::plus)
-    }
+  }
 }
 
 @Composable
-private fun RepetitionButton(repetitions: Int, onClick: (Int) -> Unit) {
-    Button(
-        onClick = { onClick(repetitions) },
-        Modifier.size(50.dp),
-        contentPadding = PaddingValues(5.dp)
-    ) {
-        Text(String.format("%+d", repetitions))
-    }
+private fun RepetitionButton(repetitions: Int, onClick: (Int) -> Unit, size: Dp) {
+  ConstrainedButton(
+    String.format("%+d", repetitions),
+    Modifier.size(size),
+    onClick = { onClick(repetitions) },
+    contentPadding = PaddingValues(5.dp)
+  )
 }
 
 @Composable
 @Preview(heightDp = 300)
 fun PreviewRepetitionButtons() {
-    MaterialTheme(Theme.lightColors) {
-        val reps = remember { Repetitions(8) }
-        Column(Modifier.fillMaxSize()) {
-            RepetitionsButtons(reps)
-        }
+  MaterialTheme(Theme.lightColors) {
+    val reps = remember { Repetitions(8) }
+    Column(Modifier.fillMaxSize()) {
+      RepetitionsButtons(reps)
     }
+  }
 }
 
 @Composable
 @Preview(heightDp = 150, widthDp = 75)
 fun PreviewSmallRepetitionButtons() {
-    MaterialTheme(Theme.lightColors) {
-        val reps = remember { Repetitions(8) }
-        Column(Modifier.fillMaxSize()) {
-            RepetitionsButtons(reps)
-        }
+  MaterialTheme(Theme.lightColors) {
+    val reps = remember { Repetitions(8) }
+    Column(Modifier.fillMaxSize()) {
+      RepetitionsButtons(reps)
     }
+  }
 }
