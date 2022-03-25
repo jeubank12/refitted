d5e3c27c515be2416e5bad1f3b0db2bcef07f01f
Josiah Eubank <eub.kansas19@gmail.com>
2022-03-24 18:51:46 -0600

    [quality] Use the constrained button in the weight buttons to make them fit

--- app/src/main/java/com/litus_animae/refitted/compose/exercise/input/WeightButtons.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/exercise/input/WeightButtons.kt
@@ -1,7 +1,6 @@
 package com.litus_animae.refitted.compose
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,8 +9,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import com.litus_animae.refitted.compose.state.Weight
+import com.litus_animae.refitted.compose.util.ConstrainedButton
 import com.litus_animae.refitted.compose.util.Theme
 import java.text.DecimalFormat
 import kotlin.math.sign
@@ -19,85 +21,97 @@ import kotlin.math.withSign
 
 @Composable
 fun WeightButtons(weight: Weight) {
-    val displayedWeight by weight.value
-    Row {
-        Column(Modifier.weight(3f)) {
-            ButtonSet(-1, weight::plus)
-        }
-        Column(
-            Modifier
-                .weight(2f)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(String.format("%.1f", displayedWeight))
-        }
-        Column(Modifier.weight(3f)) {
-            ButtonSet(1, weight::plus)
-        }
+  val displayedWeight by weight.value
+  Row {
+    Column(Modifier.weight(3f)) {
+      ButtonSet(-1, weight::plus)
     }
+    Column(
+      Modifier
+        .weight(2f)
+        .padding(16.dp),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text(String.format("%.1f", displayedWeight))
+    }
+    Column(Modifier.weight(3f)) {
+      ButtonSet(1, weight::plus)
+    }
+  }
 }
 
 @Composable
 private fun ButtonSet(sign: Int, updateWeight: (change: Double) -> Unit) {
-    Row(
+  BoxWithConstraints() {
+    // TODO if available size is too small.....
+    val maxButtonWidth = (maxWidth - 15.dp) / 2
+    val maxButtonHeight = (maxHeight - 20.dp) / 3
+    val availableSize = min(maxButtonWidth, maxButtonHeight)
+    val buttonSize = min(availableSize, 50.dp)
+    Column() {
+      Row(
         Modifier
-            .fillMaxWidth()
-            .padding(vertical = 5.dp),
+          .fillMaxWidth()
+          .padding(top = 5.dp),
         horizontalArrangement = Arrangement.SpaceAround
-    ) {
-        WeightButton(2.5.withSign(sign), updateWeight)
-        WeightButton(5.0.withSign(sign), updateWeight)
-    }
-    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-        WeightButton(10.0.withSign(sign), updateWeight)
-        WeightButton(25.0.withSign(sign), updateWeight)
-    }
-    Row(
+      ) {
+        WeightButton(2.5.withSign(sign), updateWeight, buttonSize)
+        WeightButton(5.0.withSign(sign), updateWeight, buttonSize)
+      }
+      Row(
         Modifier
-            .fillMaxWidth()
-            .padding(vertical = 5.dp),
+          .fillMaxWidth()
+          .padding(vertical = 5.dp),
         horizontalArrangement = Arrangement.SpaceAround
-    ) {
-        WeightButton(45.0.withSign(sign), updateWeight)
+      ) {
+        WeightButton(10.0.withSign(sign), updateWeight, buttonSize)
+        WeightButton(25.0.withSign(sign), updateWeight, buttonSize)
+      }
+      Row(
+        Modifier
+          .fillMaxWidth()
+          .padding(bottom = 5.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+      ) {
+        WeightButton(45.0.withSign(sign), updateWeight, buttonSize)
+      }
     }
+  }
 }
 
 @Composable
-private fun WeightButton(weight: Double, onClick: (Double) -> Unit) {
-    val sign = if (weight.sign < 0) "" else "+"
-    val df = DecimalFormat.getInstance()
-    if (df is DecimalFormat) {
-        df.applyPattern("$sign##.#")
-    }
-    Button(
-        onClick = { onClick(weight) },
-        Modifier.size(50.dp),
-        contentPadding = PaddingValues(5.dp)
-    ) {
-        Text(df.format(weight))
-    }
+private fun WeightButton(weight: Double, onClick: (Double) -> Unit, size: Dp) {
+  val sign = if (weight.sign < 0) "" else "+"
+  val df = DecimalFormat.getInstance()
+  if (df is DecimalFormat) {
+    df.applyPattern("$sign##.#")
+  }
+  ConstrainedButton(
+    df.format(weight),
+    onClick = { onClick(weight) },
+    modifier = Modifier.size(size)
+  )
 }
 
 @Composable
 @Preview(heightDp = 300)
 fun PreviewWeightButtons() {
-    MaterialTheme(Theme.lightColors) {
-        val weight = remember { Weight(10.5) }
-        Column(Modifier.fillMaxSize()) {
-            WeightButtons(weight)
-        }
+  MaterialTheme(Theme.lightColors) {
+    val weight = remember { Weight(10.5) }
+    Column(Modifier.fillMaxSize()) {
+      WeightButtons(weight)
     }
+  }
 }
 
 @Composable
-@Preview(heightDp = 150, widthDp = 300)
+@Preview(heightDp = 150, widthDp = 250)
 fun PreviewSmallWeightButtons() {
-    MaterialTheme(Theme.lightColors) {
-        val weight = remember { Weight(10.5) }
-        Column(Modifier.fillMaxSize()) {
-            WeightButtons(weight)
-        }
+  MaterialTheme(Theme.lightColors) {
+    val weight = remember { Weight(10.5) }
+    Column(Modifier.fillMaxSize()) {
+      WeightButtons(weight)
     }
+  }
 }
