5eb489ff1582e41c08067dedf00403261151e8d2
Josiah Eubank <eub.kansas19@gmail.com>
2022-03-25 08:21:53 -0600

    [exercise-set-polish] Show reps range, unit, and to failure indicator

--- app/src/main/java/com/litus_animae/refitted/compose/exercise/Detail.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/exercise/Detail.kt
@@ -23,11 +23,13 @@ import androidx.paging.PagingData
 import arrow.core.Option
 import arrow.core.getOrElse
 import com.litus_animae.refitted.R
-import com.litus_animae.refitted.compose.ExerciseSetView
 import com.litus_animae.refitted.compose.LoadingView
 import com.litus_animae.refitted.compose.state.Record
 import com.litus_animae.refitted.compose.util.Theme
-import com.litus_animae.refitted.models.*
+import com.litus_animae.refitted.models.ExerciseRecord
+import com.litus_animae.refitted.models.ExerciseSet
+import com.litus_animae.refitted.models.ExerciseViewModel
+import com.litus_animae.refitted.models.SetRecord
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
@@ -37,248 +39,259 @@ import kotlinx.coroutines.launch
 @FlowPreview
 @Composable
 fun ExerciseDetails(
-    model: ExerciseViewModel = viewModel(),
-    setHistoryList: (Flow<PagingData<SetRecord>>) -> Unit,
-    setContextMenu: (@Composable RowScope.() -> Unit) -> Unit
+  model: ExerciseViewModel = viewModel(),
+  setHistoryList: (Flow<PagingData<SetRecord>>) -> Unit,
+  setContextMenu: (@Composable RowScope.() -> Unit) -> Unit
 ) {
-    val (index, setIndex) = remember { mutableStateOf(0) }
-    val instructions by model.exercises.collectAsState(initial = emptyList(), Dispatchers.IO)
-    val instruction by derivedStateOf { instructions.getOrNull(index) }
-    val exerciseSet by instruction?.set?.collectAsState(initial = null, Dispatchers.IO)
-        ?: remember { mutableStateOf<ExerciseSet?>(null) }
-    val records = remember { mutableStateListOf<SnapshotStateList<Record>>() }
+  val (index, setIndex) = remember { mutableStateOf(0) }
+  val instructions by model.exercises.collectAsState(initial = emptyList(), Dispatchers.IO)
+  val instruction by derivedStateOf { instructions.getOrNull(index) }
+  val exerciseSet by instruction?.set?.collectAsState(initial = null, Dispatchers.IO)
+    ?: remember { mutableStateOf<ExerciseSet?>(null) }
+  val records = remember { mutableStateListOf<SnapshotStateList<Record>>() }
 
-    if (exerciseSet == null) {
-        Surface(Modifier.fillMaxSize()) {
-            LoadingView()
-        }
-    } else {
-        val currentSet = exerciseSet!!
-        var storedRecords by remember(currentSet) {
-            mutableStateOf(
-                ExerciseRecord(
-                    currentSet,
-                    emptyFlow(),
-                    emptyFlow(),
-                    emptyFlow()
-                )
-            )
-        }
-        LaunchedEffect(currentSet) {
-            setContextMenu { instruction?.let { this.ExerciseContextMenu(it) } }
-            model.recordForExercise(currentSet).collect {
-                storedRecords = it
-                setHistoryList(it.allSets)
-            }
-        }
-        // TODO update default weight with best default
-        val defaultRecord by derivedStateOf { Record(25.0, currentSet.reps, currentSet) }
-        val lastExerciseRecord by storedRecords.latestSet.collectAsState(
-            initial = null,
-            Dispatchers.IO
-        )
-        val todayExerciseRecords by storedRecords.sets.collectAsState(
-            initial = emptyList(),
-            Dispatchers.IO
-        )
-        val todayRecords by derivedStateOf {
-            todayExerciseRecords.map {
-                Record(it.weight, it.reps, currentSet, stored = true)
-            }
-        }
-        val lastStoredRecord by derivedStateOf {
-            Option.fromNullable(lastExerciseRecord).map {
-                Record(it.weight, it.reps, currentSet)
-            }.getOrElse { defaultRecord }
-        }
-        val setRecords by derivedStateOf {
-            records.getOrElse(index) {
-                if (todayRecords.isEmpty()) mutableStateListOf(lastStoredRecord)
-                else mutableStateListOf(*todayRecords.toTypedArray())
-            }
-        }
-        val currentRecord by derivedStateOf {
-            val unsavedRecord by derivedStateOf { setRecords.firstOrNull { !it.stored } }
-            val lastRecord by derivedStateOf { setRecords.last() }
-            unsavedRecord ?: lastRecord
-        }
-        val setsCompleted by storedRecords.setsCount.collectAsState(initial = 0, Dispatchers.IO)
-
-        val scope = rememberCoroutineScope()
-        DetailView(
-            index,
-            instructions.size - 1,
-            currentSet,
-            currentRecord,
-            numCompleted = setsCompleted,
-            updateIndex = { newIndex, updatedRecord ->
-                saveRecordInState(records, index, setRecords, updatedRecord)
-                setIndex(newIndex)
-            },
-            onSave = { updatedRecord ->
-                val savedRecord = updatedRecord.copy(stored = true)
-                saveRecordInState(records, index, setRecords, savedRecord)
-                scope.launch {
-                    model.saveExercise(
-                        SetRecord(
-                            savedRecord.weight,
-                            savedRecord.reps,
-                            savedRecord.set
-                        )
-                    )
-                }
-            })
+  if (exerciseSet == null) {
+    Surface(Modifier.fillMaxSize()) {
+      LoadingView()
     }
+  } else {
+    val currentSet = exerciseSet!!
+    var storedRecords by remember(currentSet) {
+      mutableStateOf(
+        ExerciseRecord(
+          currentSet,
+          emptyFlow(),
+          emptyFlow(),
+          emptyFlow()
+        )
+      )
+    }
+    LaunchedEffect(currentSet) {
+      setContextMenu { instruction?.let { this.ExerciseContextMenu(it) } }
+      model.recordForExercise(currentSet).collect {
+        storedRecords = it
+        setHistoryList(it.allSets)
+      }
+    }
+    // TODO update default weight with best default
+    val defaultRecord by derivedStateOf {
+      Record(
+        25.0,
+        if (currentSet.repsUnit.isNotBlank()) 0 else currentSet.reps,
+        currentSet
+      )
+    }
+    val lastExerciseRecord by storedRecords.latestSet.collectAsState(
+      initial = null,
+      Dispatchers.IO
+    )
+    val todayExerciseRecords by storedRecords.sets.collectAsState(
+      initial = emptyList(),
+      Dispatchers.IO
+    )
+    val todayRecords by derivedStateOf {
+      todayExerciseRecords.map {
+        Record(it.weight, it.reps, currentSet, stored = true)
+      }
+    }
+    val lastStoredRecord by derivedStateOf {
+      Option.fromNullable(lastExerciseRecord).map {
+        Record(it.weight, it.reps, currentSet)
+      }.getOrElse { defaultRecord }
+    }
+    val setRecords by derivedStateOf {
+      records.getOrElse(index) {
+        if (todayRecords.isEmpty()) mutableStateListOf(lastStoredRecord)
+        else mutableStateListOf(*todayRecords.toTypedArray())
+      }
+    }
+    val currentRecord by derivedStateOf {
+      val unsavedRecord = setRecords.firstOrNull { !it.stored }
+      val lastRecord = setRecords.last()
+      unsavedRecord ?: lastRecord
+    }
+    val setsCompleted by storedRecords.setsCount.collectAsState(initial = 0, Dispatchers.IO)
+
+    val scope = rememberCoroutineScope()
+    DetailView(
+      index,
+      instructions.size - 1,
+      currentSet,
+      currentRecord,
+      numCompleted = setsCompleted,
+      updateIndex = { newIndex, updatedRecord ->
+        saveRecordInState(records, index, setRecords, updatedRecord)
+        setIndex(newIndex)
+      },
+      onSave = { updatedRecord ->
+        val savedRecord = updatedRecord.copy(stored = true)
+        saveRecordInState(records, index, setRecords, savedRecord)
+        scope.launch {
+          model.saveExercise(
+            SetRecord(
+              savedRecord.weight,
+              savedRecord.reps,
+              savedRecord.set
+            )
+          )
+        }
+      })
+  }
 }
 
 private fun saveRecordInState(
-    records: SnapshotStateList<SnapshotStateList<Record>>,
-    index: Int,
-    setRecords: SnapshotStateList<Record>,
-    savedRecord: Record
+  records: SnapshotStateList<SnapshotStateList<Record>>,
+  index: Int,
+  setRecords: SnapshotStateList<Record>,
+  savedRecord: Record
 ) {
-    if (records.getOrNull(index) != null)
-        if (setRecords.firstOrNull { !it.stored } != null)
-            setRecords[setRecords.lastIndex] = savedRecord
-        else
-            setRecords.add(savedRecord)
+  if (records.getOrNull(index) != null)
+    if (setRecords.firstOrNull { !it.stored } != null)
+      setRecords[setRecords.lastIndex] = savedRecord
     else
-        records.add(index, mutableStateListOf(savedRecord))
+      setRecords.add(savedRecord)
+  else
+    records.add(index, mutableStateListOf(savedRecord))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewDetailView(@PreviewParameter(ExampleExerciseProvider::class) exerciseSet: ExerciseSet) {
-    MaterialTheme(Theme.darkColors) {
-        Column {
-            DetailView(
-                index = 0,
-                maxIndex = 2,
-                exerciseSet = exerciseSet,
-                record = Record(25.0, exerciseSet.reps, exerciseSet),
-                numCompleted = 1,
-                updateIndex = { _, _ -> },
-                onSave = { })
-        }
+  MaterialTheme(Theme.darkColors) {
+    Column {
+      DetailView(
+        index = 0,
+        maxIndex = 2,
+        exerciseSet = exerciseSet,
+        record = Record(25.0, exerciseSet.reps, exerciseSet),
+        numCompleted = 1,
+        updateIndex = { _, _ -> },
+        onSave = { })
     }
+  }
 }
 
 @Composable
 fun DetailView(
-    index: Int,
-    maxIndex: Int,
-    exerciseSet: ExerciseSet,
-    record: Record,
-    numCompleted: Int,
-    updateIndex: (Int, Record) -> Unit,
-    onSave: (Record) -> Unit
+  index: Int,
+  maxIndex: Int,
+  exerciseSet: ExerciseSet,
+  record: Record,
+  numCompleted: Int,
+  updateIndex: (Int, Record) -> Unit,
+  onSave: (Record) -> Unit
 ) {
-    when (LocalConfiguration.current.orientation) {
-        Configuration.ORIENTATION_LANDSCAPE ->
-            Row(Modifier.padding(16.dp)) {
-                ExerciseDetails(exerciseSet, Modifier.weight(1f))
-                ExerciseSetView(
-                    exerciseSet,
-                    record,
-                    numCompleted,
-                    index,
-                    maxIndex,
-                    updateIndex,
-                    onSave,
-                    Modifier.weight(1f)
-                )
-            }
-        else ->
-            Column(Modifier.padding(16.dp)) {
-                Row(Modifier.weight(1f)) {
-                    ExerciseDetails(exerciseSet)
-                }
-                Row(Modifier.weight(1f)) {
-                    ExerciseSetView(
-                        exerciseSet,
-                        record,
-                        numCompleted,
-                        index,
-                        maxIndex,
-                        updateIndex,
-                        onSave
-                    )
-                }
-            }
-    }
+  when (LocalConfiguration.current.orientation) {
+    Configuration.ORIENTATION_LANDSCAPE ->
+      Row(Modifier.padding(16.dp)) {
+        ExerciseDetails(exerciseSet, Modifier.weight(1f))
+        ExerciseSetView(
+          exerciseSet,
+          record,
+          numCompleted,
+          index,
+          maxIndex,
+          updateIndex,
+          onSave,
+          Modifier.weight(1f)
+        )
+      }
+    else ->
+      Column(Modifier.padding(16.dp)) {
+        Row(Modifier.weight(1f)) {
+          ExerciseDetails(exerciseSet)
+        }
+        Row(Modifier.weight(1f)) {
+          ExerciseSetView(
+            exerciseSet,
+            record,
+            numCompleted,
+            index,
+            maxIndex,
+            updateIndex,
+            onSave
+          )
+        }
+      }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewExerciseDetails(@PreviewParameter(ExampleExerciseProvider::class) exerciseSet: ExerciseSet) {
-    MaterialTheme(Theme.darkColors) {
-        Column {
-            this.ExerciseDetails(
-                exerciseSet = exerciseSet
-            )
-        }
+  MaterialTheme(Theme.darkColors) {
+    Column {
+      this.ExerciseDetails(
+        exerciseSet = exerciseSet
+      )
     }
+  }
 }
 
 @Composable
 private fun RowScope.ExerciseDetails(
-    exerciseSet: ExerciseSet?,
-    modifier: Modifier = Modifier
+  exerciseSet: ExerciseSet?,
+  modifier: Modifier = Modifier
 ) {
-    Column(modifier) {
-        ExerciseDetails(exerciseSet)
-    }
+  Column(modifier) {
+    ExerciseDetails(exerciseSet)
+  }
 }
 
 @Composable
 private fun ColumnScope.ExerciseDetails(exerciseSet: ExerciseSet?) {
-    Row {
-        if (exerciseSet != null)
-            Text(text = exerciseSet.exerciseName, style = MaterialTheme.typography.h6)
+  Row {
+    if (exerciseSet != null)
+      Text(text = exerciseSet.exerciseName, style = MaterialTheme.typography.h6)
+  }
+  Row(Modifier.padding(vertical = 5.dp)) {
+    Column(Modifier.weight(1f)) {
+      val label = stringResource(id = R.string.target_reps)
+      val target = stringResource(id = R.string.target)
+      val toFailureLabel = stringResource(id = R.string.to_failure)
+      when {
+        exerciseSet == null -> Text(label)
+        exerciseSet.reps < 0 -> Text("$label $toFailureLabel")
+        exerciseSet.repsUnit.isNotBlank() -> Text("$target ${exerciseSet.reps} ${exerciseSet.repsUnit}")
+        exerciseSet.repsRange > 0 && !exerciseSet.isToFailure -> Text("$label ${exerciseSet.reps}-${exerciseSet.reps + exerciseSet.repsRange}")
+        exerciseSet.repsRange > 0 && exerciseSet.isToFailure -> Text("$label ${exerciseSet.reps}-${exerciseSet.reps + exerciseSet.repsRange} ($toFailureLabel)")
+        exerciseSet.isToFailure -> Text("$label ${exerciseSet.reps} ($toFailureLabel)")
+        else -> Text("$label ${exerciseSet.reps}")
+      }
     }
-    Row(Modifier.padding(vertical = 5.dp)) {
-        Column(Modifier.weight(1f)) {
-            val label = stringResource(id = R.string.target_reps)
-            val toFailureLabel = stringResource(id = R.string.to_failure)
-            when {
-                exerciseSet == null -> Text(label)
-                exerciseSet.reps < 0 -> Text("$label $toFailureLabel")
-                else -> Text("$label ${exerciseSet.reps}")
-            }
-        }
-        Column(Modifier.weight(1f)) {
-            val label = stringResource(id = R.string.target_sets)
-            if (exerciseSet == null)
-                Text(label)
-            else Text("$label ${exerciseSet.sets}")
-        }
+    Column(Modifier.weight(1f)) {
+      val label = stringResource(id = R.string.target_sets)
+      if (exerciseSet == null)
+        Text(label)
+      else Text("$label ${exerciseSet.sets}")
     }
-    val scrollState = rememberScrollState()
-    Row {
-        // TODO is there a way to show the scrollbar to indicate scrollability?
-        Column(Modifier.verticalScroll(scrollState)) {
-            Row(Modifier.padding(vertical = 5.dp)) {
-                if (exerciseSet != null) {
-                    val exercise by exerciseSet.exercise.collectAsState(null, Dispatchers.IO)
-                    Text(exercise?.description ?: "")
-                }
-            }
-            Row(Modifier.padding(vertical = 5.dp)) {
-                Text(exerciseSet?.note ?: "")
-            }
+  }
+  val scrollState = rememberScrollState()
+  Row {
+    // TODO is there a way to show the scrollbar to indicate scrollability?
+    Column(Modifier.verticalScroll(scrollState)) {
+      Row(Modifier.padding(vertical = 5.dp)) {
+        if (exerciseSet != null) {
+          val exercise by exerciseSet.exercise.collectAsState(null, Dispatchers.IO)
+          Text(exercise?.description ?: "")
         }
+      }
+      Row(Modifier.padding(vertical = 5.dp)) {
+        Text(exerciseSet?.note ?: "")
+      }
     }
+  }
 }
 
 @Composable
 fun RowScope.ExerciseContextMenu(instruction: ExerciseViewModel.ExerciseInstruction) {
-    if (instruction.hasAlternate) {
-        Column(Modifier
-            .clickable { instruction.activateNextAlternate() }
-            .fillMaxHeight(),
-            verticalArrangement = Arrangement.Center) {
-            // TODO localize
-            Text("Alternate", Modifier.padding(end = 5.dp))
-        }
+  if (instruction.hasAlternate) {
+    Column(Modifier
+      .clickable { instruction.activateNextAlternate() }
+      .fillMaxHeight(),
+      verticalArrangement = Arrangement.Center) {
+      val altLabel = stringResource(id = R.string.alternate)
+      Text(altLabel, Modifier.padding(end = 5.dp))
     }
+  }
 }
--- app/src/main/java/com/litus_animae/refitted/compose/state/Repetitions.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/state/Repetitions.kt
@@ -3,13 +3,14 @@ package com.litus_animae.refitted.compose.state
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import kotlin.math.absoluteValue
+import kotlin.math.max
 import kotlin.math.sign
 
 class Repetitions(initialValue: Int) {
-    private var reps = mutableStateOf(initialValue)
-    val value: State<Int> = reps
-    fun plus(change: Int) {
-        if (change.sign < 0 && reps.value < change.absoluteValue) reps.value = 0
-        else reps.value += change
-    }
+  private var reps = mutableStateOf(max(0, initialValue))
+  val value: State<Int> = reps
+  fun plus(change: Int) {
+    if (change.sign < 0 && reps.value < change.absoluteValue) reps.value = 0
+    else reps.value += change
+  }
 }
