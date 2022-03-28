cc16239ec9fd762eb5f9b167c16b3dcbbaff1be9
Josiah Eubank <eub.kansas19@gmail.com>
2022-03-28 07:57:43 -0600

    [exercise-polish-2] Show actual current timer when switching between pages and not running

--- app/src/main/java/com/litus_animae/refitted/compose/exercise/RestTimer.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/exercise/RestTimer.kt
@@ -22,116 +22,117 @@ import java.time.Instant
 
 @Composable
 fun Timer(
-    running: Boolean,
-    millisToElapse: Long,
-    resolutionMillis: Long = 100,
-    countDown: Boolean = false,
-    debugView: Boolean = false,
-    onUpdate: (Long) -> Unit = {},
-    onFinish: () -> Unit = {}
+  running: Boolean,
+  millisToElapse: Long,
+  resolutionMillis: Long = 100,
+  countDown: Boolean = false,
+  debugView: Boolean = false,
+  onUpdate: (Long) -> Unit = {},
+  onFinish: () -> Unit = {}
 ) {
-    val startTime = rememberSaveable(running) {
-        Instant.now()
-    }
-    val runtimeMillis = rememberSaveable(running) {
-        millisToElapse
-    }
-    var isRunning by rememberSaveable(running) { mutableStateOf(running) }
-    val timerScope = rememberCoroutineScope()
-    var elapsedMillis by rememberSaveable(running) { mutableStateOf(0L) }
-    LaunchedEffect(running, elapsedMillis) {
-        if (isRunning) {
-            timerScope.launch {
-                delay(resolutionMillis)
-                val nowElapsed = Instant.now().toEpochMilli() - startTime.toEpochMilli()
-                if (nowElapsed > runtimeMillis) {
-                    isRunning = false
-                    elapsedMillis = runtimeMillis
-                    onFinish()
-                } else {
-                    elapsedMillis = nowElapsed
-                }
-                onUpdate(if (countDown) runtimeMillis - elapsedMillis else elapsedMillis)
-            }
+  val startTime = rememberSaveable(running) {
+    Instant.now()
+  }
+  val runtimeMillis = rememberSaveable(running) {
+    millisToElapse
+  }
+  var isRunning by rememberSaveable(running) { mutableStateOf(running) }
+  val timerScope = rememberCoroutineScope()
+  var elapsedMillis by rememberSaveable(running) { mutableStateOf(0L) }
+  val millisToShow = if (isRunning) runtimeMillis else millisToElapse
+  LaunchedEffect(running, elapsedMillis) {
+    if (isRunning) {
+      timerScope.launch {
+        delay(resolutionMillis)
+        val nowElapsed = Instant.now().toEpochMilli() - startTime.toEpochMilli()
+        if (nowElapsed > runtimeMillis) {
+          isRunning = false
+          elapsedMillis = runtimeMillis
+          onFinish()
+        } else {
+          elapsedMillis = nowElapsed
         }
+        onUpdate(if (countDown) runtimeMillis - elapsedMillis else elapsedMillis)
+      }
     }
-    if (debugView) {
-        Column {
-            drawTimer(runtimeMillis, elapsedMillis, countDown)
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                Text("Running: $running")
-                Text("IsRunning: $isRunning")
-                Text("millisToElapse: $runtimeMillis")
-                Text("elapsedMillis: $elapsedMillis")
-            }
-        }
-    } else {
-        drawTimer(runtimeMillis, elapsedMillis, countDown)
+  }
+  if (debugView) {
+    Column {
+      drawTimer(millisToShow, elapsedMillis, countDown)
+      Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Text("Running: $running")
+        Text("IsRunning: $isRunning")
+        Text("millisToElapse: $runtimeMillis")
+        Text("elapsedMillis: $elapsedMillis")
+      }
     }
+  } else {
+    drawTimer(millisToShow, elapsedMillis, countDown)
+  }
 }
 
 @Composable
 private fun drawTimer(millisToElapse: Long, elapsedMillis: Long, countDown: Boolean) {
-    val drawColor = MaterialTheme.colors.onSurface
-    val elapsedColor = MaterialTheme.colors.primary
-    Canvas(
-        Modifier
-            .fillMaxWidth()
-            .height(10.dp)
-            .background(MaterialTheme.colors.surface)
-    ) {
-        val offsetMillis = if (countDown) millisToElapse - elapsedMillis else elapsedMillis
-        val elapsedOffsetX = (size.width - 6f) / millisToElapse * offsetMillis
-        val startOffset = Offset(3f, size.height / 2f)
-        val endOffset = Offset(size.width - 3f, size.height / 2f)
-        val elapsedOffset = startOffset.plus(Offset(elapsedOffsetX, 0f))
-        if (startOffset != elapsedOffset) {
-            drawLine(
-                elapsedColor,
-                start = startOffset,
-                end = elapsedOffset,
-                strokeWidth = 10f
-            )
-        }
-        drawLine(
-            drawColor,
-            start = elapsedOffset,
-            end = endOffset,
-            strokeWidth = 10f
-        )
+  val drawColor = MaterialTheme.colors.onSurface
+  val elapsedColor = MaterialTheme.colors.primary
+  Canvas(
+      Modifier
+          .fillMaxWidth()
+          .height(10.dp)
+          .background(MaterialTheme.colors.surface)
+  ) {
+    val offsetMillis = if (countDown) millisToElapse - elapsedMillis else elapsedMillis
+    val elapsedOffsetX = (size.width - 6f) / millisToElapse * offsetMillis
+    val startOffset = Offset(3f, size.height / 2f)
+    val endOffset = Offset(size.width - 3f, size.height / 2f)
+    val elapsedOffset = startOffset.plus(Offset(elapsedOffsetX, 0f))
+    if (startOffset != elapsedOffset) {
+      drawLine(
+        elapsedColor,
+        start = startOffset,
+        end = elapsedOffset,
+        strokeWidth = 10f
+      )
     }
+    drawLine(
+      drawColor,
+      start = elapsedOffset,
+      end = endOffset,
+      strokeWidth = 10f
+    )
+  }
 }
 
 class ElapsedMillisParameterProvider : PreviewParameterProvider<Long> {
-    override val values: Sequence<Long> = sequenceOf(
-        0L, 10000L, 30000L, 59000L, 60000L
-    )
+  override val values: Sequence<Long> = sequenceOf(
+    0L, 10000L, 30000L, 59000L, 60000L
+  )
 }
 
 @Composable
 @Preview(widthDp = 800)
 fun PreviewTimer(@PreviewParameter(ElapsedMillisParameterProvider::class) elapsedMillis: Long) {
-    MaterialTheme(Theme.lightColors) {
-        drawTimer(60000L, elapsedMillis, countDown = false)
-    }
+  MaterialTheme(Theme.lightColors) {
+    drawTimer(60000L, elapsedMillis, countDown = false)
+  }
 }
 
 @Composable
 @Preview(widthDp = 800)
 fun PreviewRunningTimer() {
-    var running by remember { mutableStateOf(false) }
-    var down by remember { mutableStateOf(false) }
-    MaterialTheme(Theme.lightColors) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Timer(running, 15000L, debugView = true, countDown = down) { running = false }
-            Row {
-                Button(onClick = { running = !running }) {
-                    Text(running.toString())
-                }
-                Button(onClick = { down = !down }) {
-                    Text(if (down) "down" else "up")
-                }
-            }
+  var running by remember { mutableStateOf(false) }
+  var down by remember { mutableStateOf(false) }
+  MaterialTheme(Theme.lightColors) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+      Timer(running, 15000L, debugView = true, countDown = down) { running = false }
+      Row {
+        Button(onClick = { running = !running }) {
+          Text(running.toString())
         }
+        Button(onClick = { down = !down }) {
+          Text(if (down) "down" else "up")
+        }
+      }
     }
+  }
 }
