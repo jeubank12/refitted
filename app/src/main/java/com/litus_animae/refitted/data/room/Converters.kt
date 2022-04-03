7fabf657e5a0b5232b456eb786a47419e1b3b394
Josiah Eubank <eub.kansas19@gmail.com>
2022-04-03 11:13:31 -0600

    [rest-on-calendar] Add rest days to workout plan model

--- app/src/main/java/com/litus_animae/refitted/data/room/Converters.kt
+++ app/src/main/java/com/litus_animae/refitted/data/room/Converters.kt
@@ -4,15 +4,27 @@ import androidx.room.TypeConverter
 import java.util.*
 
 object Converters {
-    @JvmStatic
-    @TypeConverter
-    fun fromTimestamp(value: Long?): Date? {
-        return if (value == null) null else Date(value)
-    }
+  @JvmStatic
+  @TypeConverter
+  fun fromTimestamp(value: Long?): Date? {
+    return value?.let { Date(it) }
+  }
 
-    @JvmStatic
-    @TypeConverter
-    fun dateToTimestamp(date: Date?): Long? {
-        return date?.time
-    }
+  @JvmStatic
+  @TypeConverter
+  fun dateToTimestamp(date: Date?): Long? {
+    return date?.time
+  }
+
+  @JvmStatic
+  @TypeConverter
+  fun intListFromString(value: String?): List<Int>? {
+    return value?.split(",")?.mapNotNull { it.toIntOrNull() }
+  }
+
+  @JvmStatic
+  @TypeConverter
+  fun intListToString(values: List<Int>?): String? {
+    return values?.joinToString(",")
+  }
 }
