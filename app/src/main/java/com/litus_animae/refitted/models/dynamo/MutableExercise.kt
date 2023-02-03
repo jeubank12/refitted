c233813404cf00efa674b50db96f3cfd6186f432
Josiah Eubank <eub.kansas19@gmail.com>
2023-02-02 17:48:09 -0700

    [exercise-as-query] Read exercises from the reverse index to fit in with IAM policy

--- app/src/main/java/com/litus_animae/refitted/models/dynamo/MutableExercise.kt
+++ app/src/main/java/com/litus_animae/refitted/models/dynamo/MutableExercise.kt
@@ -2,15 +2,19 @@ package com.litus_animae.refitted.models.dynamo
 
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable
 
 @DynamoDBTable(tableName = "refitted-exercise")
 data class MutableExercise @JvmOverloads constructor(
+  @get:DynamoDBIndexHashKey(attributeName = "Disc", globalSecondaryIndexName = "Reverse-index")
   @get:DynamoDBAttribute(attributeName = "Disc")
   @get:DynamoDBRangeKey(attributeName = "Disc")
   var workout: String = "",
 
+  @get:DynamoDBIndexRangeKey(attributeName = "Id", globalSecondaryIndexName = "Reverse-index")
   @get:DynamoDBAttribute(attributeName = "Id")
   @get:DynamoDBHashKey(attributeName = "Id")
   var id: String = "",
