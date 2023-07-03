fb6402dce244252d8f1d24f90d977f3d5be29122
Josiah Eubank <eub.kansas19@gmail.com>
2023-07-03 10:45:09 -0600

    [fix-rotation-lost-state] Remember repetitions through rotation

--- app/src/main/java/com/litus_animae/refitted/compose/state/Repetitions.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/state/Repetitions.kt
@@ -1,11 +1,14 @@
 package com.litus_animae.refitted.compose.state
 
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.mapSaver
 import kotlin.math.absoluteValue
 import kotlin.math.max
 import kotlin.math.sign
 
+@Stable
 class Repetitions(initialValue: Int) {
   private var reps = mutableStateOf(max(0, initialValue))
   val value: State<Int> = reps
@@ -15,5 +18,15 @@ class Repetitions(initialValue: Int) {
   }
   fun set(value: Int) {
     reps.value = max(value, 0)
+  }
+
+  companion object {
+    val Saver = run {
+      val repsKey = "reps"
+      mapSaver(
+        save = { mapOf(repsKey to it.value.value) },
+        restore = { Repetitions(it[repsKey] as Int) }
+      )
+    }
   }
 }
