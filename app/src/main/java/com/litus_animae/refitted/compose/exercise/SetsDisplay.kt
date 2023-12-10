04b150c3a188d1753524a8d89455f3c368646617
Josiah Eubank <eub.kansas19@gmail.com>
2023-12-10 16:05:11 -0700

    [sets-from-1] Add animations for better changes in reps/sets text

--- app/src/main/java/com/litus_animae/refitted/compose/exercise/SetsDisplay.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/exercise/SetsDisplay.kt
@@ -1,21 +1,27 @@
 package com.litus_animae.refitted.compose.exercise
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import arrow.core.getOrElse
 import com.litus_animae.refitted.models.ExerciseSet
 import com.litus_animae.refitted.models.Record
-import com.litus_animae.refitted.util.MonadUtil
 
 @Composable
 fun SetsDisplay(
@@ -31,35 +37,70 @@ fun SetsDisplay(
       verticalArrangement = Arrangement.Center,
       horizontalAlignment = Alignment.CenterHorizontally
     ) {
-      val toCompletionSetPhrase = MonadUtil.optionWhen(exerciseSet.sets < 0) {
-        val expectedReps = exerciseSet.reps(numCompleted)
-        if (expectedReps < 0) {
-          // This is a "complete as many as possible" set
-          "${record.cumulativeReps}"
-        } else {
-          // This is a "complete this many reps in as many sets as you need" set
-          "${expectedReps - record.cumulativeReps} / $expectedReps"
-        } to "Reps Completed"
+      val expectedReps = exerciseSet.reps(numCompleted)
+      val progressText = when {
+        exerciseSet.sets < 0 && expectedReps < 0 -> "${record.cumulativeReps}"
+        exerciseSet.sets < 0 -> "${expectedReps - record.cumulativeReps}"
+        numCompleted >= exerciseSet.sets -> "$numCompleted"
+        else -> "${numCompleted + 1}"
       }
-      val (setDisplay, setSubtext) = toCompletionSetPhrase
-        .getOrElse {
-          if (numCompleted >= exerciseSet.sets) "$numCompleted" to "Sets Completed"
-          else "${numCompleted + 1} / ${exerciseSet.sets}" to "Sets Completed"
+      val showProgressGoal = when {
+        exerciseSet.sets < 0 -> expectedReps >= 0
+        else -> numCompleted < exerciseSet.sets
+      }
+      val progressGoal =
+        remember { mutableIntStateOf(if (exerciseSet.sets < 0) expectedReps else exerciseSet.sets) }
+      LaunchedEffect(exerciseSet) {
+        if (exerciseSet.sets < 0) {
+          if (expectedReps >= 0) progressGoal.intValue = expectedReps
+        } else if (numCompleted < exerciseSet.sets) {
+          progressGoal.intValue = exerciseSet.sets
         }
+      }
+      val showAsCompleted = when {
+        exerciseSet.sets < 0 -> true
+        else -> numCompleted >= exerciseSet.sets
+      }
+
       AnimatedVisibility(
-        toCompletionSetPhrase.isNone() && numCompleted < exerciseSet.sets,
+        !showAsCompleted,
         enter = expandVertically(),
         exit = shrinkVertically()
       ) {
         Text("Set", style = MaterialTheme.typography.h5)
       }
-      Text(setDisplay, style = MaterialTheme.typography.h4)
+      Row {
+        Text(progressText, style = MaterialTheme.typography.h4)
+        AnimatedVisibility(
+          showProgressGoal,
+          enter = expandHorizontally(expandFrom = Alignment.Start),
+          exit = shrinkHorizontally(shrinkTowards = Alignment.Start)
+        ) {
+          Text(" / ${progressGoal.intValue}", style = MaterialTheme.typography.h4)
+        }
+      }
       AnimatedVisibility(
-        !(toCompletionSetPhrase.isNone() && numCompleted < exerciseSet.sets),
+        showAsCompleted,
         enter = expandVertically(expandFrom = Alignment.Top),
         exit = shrinkVertically(shrinkTowards = Alignment.Top)
       ) {
-        Text(setSubtext, style = MaterialTheme.typography.h5)
+        Row {
+          AnimatedVisibility(
+            exerciseSet.sets < 0,
+            enter = fadeIn(),
+            exit = fadeOut()
+          ) {
+            Text("Reps", style = MaterialTheme.typography.h5)
+          }
+          AnimatedVisibility(
+            exerciseSet.sets >= 0,
+            enter = fadeIn(),
+            exit = fadeOut()
+          ) {
+            Text("Sets", style = MaterialTheme.typography.h5)
+          }
+          Text(" Completed", style = MaterialTheme.typography.h5)
+        }
       }
     }
   }
