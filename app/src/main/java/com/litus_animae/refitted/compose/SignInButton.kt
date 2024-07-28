f244ce34212bf698344862a2f26971d67f7b662e
Josiah Eubank <eub.kansas19@gmail.com>
2024-07-28 11:23:20 -0600

    [login-optional] Implement custom Sign in with Google button
    Branding guidelines https://developers.google.com/identity/branding-guidelines#custom-button

--- app/src/main/java/com/litus_animae/refitted/compose/SignInButton.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/SignInButton.kt
@@ -1,16 +1,32 @@
 package com.litus_animae.refitted.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
 import androidx.credentials.exceptions.GetCredentialException
-import com.google.android.gms.common.SignInButton
 import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.litus_animae.refitted.R
 import kotlinx.coroutines.launch
@@ -36,11 +52,15 @@ fun SignInButton(
 
   val coroutineScope = rememberCoroutineScope()
 
-  AndroidView(factory = ::SignInButton,
-    modifier = modifier,
-    update = { button ->
-      button.setSize(SignInButton.SIZE_WIDE)
-      button.setOnClickListener {
+  Row(
+    Modifier
+      .clip(RoundedCornerShape(20.dp))
+      .border(0.0f.dp, Color.Black, RoundedCornerShape(20.dp))
+      .background(Color.White)
+      .clickable(
+        onClickLabel = "Sign in with Google",
+        role = Role.Button
+      ) {
         coroutineScope.launch {
           try {
             val result = credentialManager.getCredential(
@@ -53,5 +73,28 @@ fun SignInButton(
           }
         }
       }
-    })
+      .padding(end = 12.dp),
+    verticalAlignment = Alignment.CenterVertically
+  ) {
+    Icon(
+      painter = painterResource(id = R.drawable.google_icon_g),
+      "Sign in with Google",
+      tint = Color.Unspecified
+    )
+    Text(
+      "Sign in with Google",
+      fontSize = 14.sp,
+      lineHeight = 20.sp,
+      fontFamily = FontFamily(Font(R.font.roboto_medium)),
+      color = Color(0xFF1F1F1F)
+    )
+  }
+}
+
+@Preview
+@Composable
+private fun SignInButtonPreview() {
+  Row(Modifier.background(Color.LightGray)) {
+    SignInButton(handleSuccess = {}) {}
+  }
 }
