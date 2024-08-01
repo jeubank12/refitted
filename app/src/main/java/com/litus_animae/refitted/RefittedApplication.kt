40c717b61bb3271c77d2193d395801658a46e2c3
Josiah Eubank <eub.kansas19@gmail.com>
2024-08-01 17:36:38 -0600

    [performance-optimization] Temporarily enable strict mode

--- app/src/main/java/com/litus_animae/refitted/RefittedApplication.kt
+++ app/src/main/java/com/litus_animae/refitted/RefittedApplication.kt
@@ -1,10 +1,36 @@
 package com.litus_animae.refitted
 
 import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class RefittedApplication : Application() {
+
+    override fun onCreate() {
+
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork() // or .detectAll() for all detectable problems
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
+
+        super.onCreate()
+    }
 
     companion object {
         private const val TAG = "RefittedApplication"
