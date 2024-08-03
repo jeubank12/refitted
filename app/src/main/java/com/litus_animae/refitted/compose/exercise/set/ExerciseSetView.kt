5dec49e4f1b72bae234bf8f8824a87d852929a07
Josiah Eubank <eub.kansas19@gmail.com>
2024-08-03 16:35:39 -0600

    [performance-optimization-recompose] Reduce recompose by breaking out RestTimer

--- app/src/main/java/com/litus_animae/refitted/compose/exercise/set/ExerciseSetView.kt
+++ app/src/main/java/com/litus_animae/refitted/compose/exercise/set/ExerciseSetView.kt
@@ -1,8 +1,5 @@
 package com.litus_animae.refitted.compose.exercise.set
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.snap
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -17,7 +14,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
@@ -27,28 +23,21 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import arrow.core.Option
-import arrow.core.getOrElse
 import com.litus_animae.refitted.R
 import com.litus_animae.refitted.compose.exercise.RepsDisplay
 import com.litus_animae.refitted.compose.exercise.SetsDisplay
-import com.litus_animae.refitted.compose.exercise.Timer
 import com.litus_animae.refitted.compose.exercise.WeightDisplay
-import com.litus_animae.refitted.compose.exercise.animateTimer
 import com.litus_animae.refitted.compose.exercise.exampleExerciseSet
 import com.litus_animae.refitted.compose.state.ExerciseSetWithRecord
 import com.litus_animae.refitted.compose.state.Repetitions
 import com.litus_animae.refitted.compose.state.Weight
 import com.litus_animae.refitted.compose.util.Theme
 import com.litus_animae.refitted.models.Record
-import com.litus_animae.refitted.util.MonadUtil.optionWhen
 import kotlinx.coroutines.flow.emptyFlow
 import java.time.Instant
-import kotlin.math.min
 
 @Composable
 fun ExerciseSetView(
@@ -90,34 +79,16 @@ fun ColumnScope.ExerciseSetView(
     saver = Repetitions.Saver,
     inputs = arrayOf(exerciseSet, record)
   ) { Repetitions(if (exerciseSet.repsAreSequenced) setWithRecord.reps else record.reps) }
+
   val timerRunning = rememberSaveable { mutableStateOf(false) }
-  val timerStart = rememberSaveable { mutableStateOf(Instant.now()) }
   val timerDuration = rememberSaveable { mutableIntStateOf(exerciseSet.rest * 1000) }
   LaunchedEffect(timerRunning.value, exerciseSet) {
     if (!timerRunning.value) {
       timerDuration.intValue = exerciseSet.rest * 1000
     }
   }
-  val timerElapsed = remember {
-    Animatable(
-      if (timerRunning.value)
-        min(
-          timerDuration.intValue.toLong(),
-          timerStart.value.toEpochMilli() + timerDuration.intValue - Instant.now().toEpochMilli()
-        ).toFloat()
-      else timerDuration.intValue.toFloat()
-    )
-  }
-  LaunchedEffect(timerRunning.value) {
-    animateTimer(
-      timerRunning.value,
-      timerDuration.intValue,
-      timerStart.value,
-      timerElapsed,
-      whilePausedAnimationSpec = snap(),
-      pausedTarget = timerDuration.intValue
-    )
-  }
+  val timerStart = rememberSaveable { mutableStateOf(Instant.now()) }
+
   val saveWeight by weight.value
   val saveReps by reps.value
 
@@ -154,73 +125,50 @@ fun ColumnScope.ExerciseSetView(
   val isTimerRunning by timerRunning
   Row(
     Modifier.height(80.dp),
-    verticalAlignment = Alignment.CenterVertically
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.SpaceBetween
   ) {
-    Column(
-      Modifier
-        .weight(1f)
-        .fillMaxWidth(),
-      verticalArrangement = Arrangement.Center
+    val moveLeftEnabled = currentIndex > 0
+    Button(
+      onClick = {
+        updateIndex(
+          currentIndex - 1,
+          record.copy(weight = saveWeight, reps = saveReps)
+        )
+      },
+      enabled = moveLeftEnabled
     ) {
-      val enabled = currentIndex > 0
-      Button(
-        onClick = {
-          updateIndex(
-            currentIndex - 1,
-            record.copy(weight = saveWeight, reps = saveReps)
-          )
-        },
-        enabled = enabled
-      ) {
-        val text = stringResource(id = R.string.move_left)
-        Text(text)
-      }
+      val text = stringResource(id = R.string.move_left)
+      Text(text)
     }
-    Column(
-      Modifier.weight(3f),
-      horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-      val localRestFormat = stringResource(R.string.seconds_rest_phrase)
-      val runningDisplayTime by remember {
-        derivedStateOf {
-          String.format(localRestFormat, timerElapsed.value / 1000)
-        }
-      }
-      val timerDisplayTime =
-          if (timerRunning.value) runningDisplayTime
-          else String.format(localRestFormat, exerciseSet.rest.toFloat())
-      Text(timerDisplayTime, style = MaterialTheme.typography.h4)
-      AnimatedVisibility(isTimerRunning || exerciseSet.rest > 0) {
-        Timer(
-          timerStart.value,
-          isTimerRunning,
-          durationMillis = timerDuration.intValue,
-          countDown = true
-        ) {
-          timerRunning.value = false
-          timerDuration.intValue = exerciseSet.rest * 1000
-        }
-      }
-    }
-    Column(
+
+    RestTimer(
       Modifier
-        .weight(1f)
-        .fillMaxWidth(),
-      horizontalAlignment = Alignment.End
+        .padding(horizontal = 15.dp)
+        .weight(3f),
+      isTimerRunning,
+      timerDuration.intValue,
+      timerStart.value,
+      timerRunning,
+      exerciseSet
     ) {
-      val enabled = currentIndex < maxIndex
-      Button(
-        onClick = {
-          updateIndex(
-            currentIndex + 1,
-            record.copy(weight = saveWeight, reps = saveReps)
-          )
-        },
-        enabled = enabled
-      ) {
-        val text = stringResource(id = R.string.move_right)
-        Text(text)
-      }
+      timerRunning.value = false
+      timerDuration.intValue = exerciseSet.rest * 1000
+    }
+
+
+    val moveRightEnabled = currentIndex < maxIndex
+    Button(
+      onClick = {
+        updateIndex(
+          currentIndex + 1,
+          record.copy(weight = saveWeight, reps = saveReps)
+        )
+      },
+      enabled = moveRightEnabled
+    ) {
+      val text = stringResource(id = R.string.move_right)
+      Text(text)
     }
   }
 
@@ -247,7 +195,7 @@ fun ColumnScope.ExerciseSetView(
 
 @Composable
 @Preview(heightDp = 400)
-fun PreviewExerciseSetDetails() {
+private fun PreviewExerciseSetDetails() {
   var numCompleted by remember { mutableIntStateOf(0) }
   var currentIndex by remember { mutableIntStateOf(5) }
   val records = remember { mutableStateListOf<Record>() }
