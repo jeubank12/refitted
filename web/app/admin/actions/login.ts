baaaf490879284b3eba33935698724101cf7c796
Josiah Eubank <eub.kansas19@gmail.com>
2024-11-05 07:31:20 -0700

    Use server action to set login session

---
+++ web/app/admin/actions/login.ts
@@ -0,0 +1,44 @@
+'use server'
+
+import { cookies } from 'next/headers'
+
+import { initializeServerApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+import { firebaseConfig } from 'src/lib/firebase/firebaseConfig'
+
+async function createSession(isAdmin: boolean) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+  const sessionContent = JSON.stringify({
+    isAdmin,
+  })
+  await cookies().set('session', sessionContent, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: 'lax',
+    path: '/',
+  })
+}
+
+export async function login(idToken: string) {
+  const firebaseServerApp = initializeServerApp(
+    firebaseConfig,
+    idToken
+      ? {
+          authIdToken: idToken,
+        }
+      : {}
+  )
+
+  const auth = getAuth(firebaseServerApp)
+  await auth.authStateReady()
+
+  // TODO 404?
+  if (!auth.currentUser) return
+  const idTokenResult = await auth.currentUser.getIdTokenResult()
+  createSession(!!idTokenResult.claims?.admin)
+  console.log('Logged in', auth.currentUser.email, {
+    isAdmin: !!idTokenResult.claims?.admin,
+  })
+}
