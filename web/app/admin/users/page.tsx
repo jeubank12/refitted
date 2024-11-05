9239f0995cc8480d83e43ad40630efebbb516893
Josiah Eubank <eub.kansas19@gmail.com>
2024-11-05 15:26:06 -0700

    [website-release] Use firebase admin directly

--- web/app/admin/users/page.tsx
+++ web/app/admin/users/page.tsx
@@ -2,7 +2,7 @@ import { listAllUsers } from 'src/lib/aws/actions/lambda'
 import ListUsersTable from './ListUsersTable'
 
 export default async function UsersList() {
-  const data = await listAllUsers().then(result => result.data)
+  const data = await listAllUsers()
 
   if (!data?.users.length) return <div>empty</div>
   else return <ListUsersTable users={data.users} />
