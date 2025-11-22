9f1fae0a97701f37f0b0f28606f9ac7a372c3c9e
Josiah Eubank <eub.kansas19@gmail.com>
2025-11-22 11:14:54 -0700

    Resolve the remaining buildHealth warnings

--- app/src/androidTest/kotlin/com/litus_animae/refitted/data/TestDataBuilder.kt
+++ app/src/androidTest/kotlin/com/litus_animae/refitted/data/TestDataBuilder.kt
@@ -10,6 +10,7 @@ import java.time.Instant
  * Test data builder for creating sample exercises, workout plans, and records
  * for integration tests.
  */
+// TODO move to room and use in RoomFromDomainConversionTest?
 object TestDataBuilder {
 
     const val TEST_WORKOUT = "TestWorkout"
